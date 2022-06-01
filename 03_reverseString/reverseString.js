39e91a55b8dda7e526b0d57d0edb87854a97eff1
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-01 13:03:26 +0200

    Pass test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    array.reverse();
+    const output = array.join("");
+    return output;
 };
 
 // Do not edit below this line
