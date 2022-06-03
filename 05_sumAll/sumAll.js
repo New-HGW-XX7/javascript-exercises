a47bcbec1e7e2a11f98c8528c6e47630786666cf
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-03 12:40:53 +0200

    Pass test 4, 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,25 @@
 const sumAll = function(a, b) {
     let sum = 0;
-    for(let i = b; i > a; i--) {
+    let greaterNum;
+    let smallerNum;
+
+    if(a < 0 || typeof a !== "number" || b < 0 || typeof b !== "number") {
+        return "ERROR";
+    };
+
+    if(b > a) {
+         greaterNum = b;
+         smallerNum = a;
+    }
+    else {
+         greaterNum = a;
+         smallerNum = b;
+    }
+
+    for(let i = greaterNum; i > smallerNum; i--) {
         sum += i;
     }
-    return sum + a;
+    return sum + smallerNum;
 };
 
 // Do not edit below this line
