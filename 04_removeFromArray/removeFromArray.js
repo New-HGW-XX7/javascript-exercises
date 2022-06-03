534b24344f765f88d562b5c972cf1ff294cabe38
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-03 11:55:46 +0200

    Pass tests 2, 3, 4, 5, 6, 7
    Learning about rest parameters made things easier.
    Remembered to decrease i upon successful slice
    in order not to miss an index.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,9 @@
-const removeFromArray = function(arr, value) {
+const removeFromArray = function(arr, ...values) {
     
     for(let i = 0; i < arr.length; i++) {
-      if (arr[i] === value) {
+      if (values.includes(arr[i]) === true) {
         arr.splice(i, 1);
+        i--;
         }
     }
 
