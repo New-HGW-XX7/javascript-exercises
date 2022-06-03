2625d3875bd487a772c395e5f8d57d44cc13d979
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-03 13:21:26 +0200

    Pass test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fValue) {
+  return (fValue - 32) * (5 / 9);
 };
 
-const ctof = function() {
-
+const ctof = function(cValue) {
+  return cValue * (9 / 5) + 32
 };
 
 // Do not edit below this line
