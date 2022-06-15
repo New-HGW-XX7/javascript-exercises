12460ae56f90f8bef5e98301926a941ac9925ba4
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-15 17:53:08 +0200

    Pass all tests
    Took me a long while to remember that arrays are objects.
    Or rather to remember what that means in terms of manipulation.
    
    Reversing the strings itself worked quite well from memory.
    Filtering the arrays only needed a quick search for syntax.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let newString = string.toLowerCase();
+
+    if(newString[newString.length - 1] === '.' || newString[newString.length - 1] === '!') {
+        newString = newString.slice(0, newString.length - 1);
+    }
+
+    let reverse = ``;
+    for(let i = newString.length - 1; i >= 0; i--) {
+        reverse = reverse + newString[i];
+     }
+
+    let comp1 = Array.from(newString);
+    let comp2 = Array.from(reverse);
+
+    let newComp1 = comp1.filter(item => item != ' ');
+    let newComp2 = comp2.filter(item => item != ' ');
+    let final1 = newComp1.filter(item => item != ',');
+    let final2 = newComp2.filter(item => item != ',');
+
+    if(final1.join('') === final2.join('')) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
