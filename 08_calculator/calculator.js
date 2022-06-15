2577df5423f0905f76a08d86c851bfac3b21c469
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-15 15:04:55 +0200

    Pass test 12, 13, 14, 15 and 16

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -24,8 +24,23 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total;
+  let innerResult;
+  if(n === 0 || n === 1) {
+    total = 1;
+  } else {
+    let arr = [];
+    for(let i = 1; i <= n; i++) {
+      arr.push(i);
+    }
+
+    total = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+    total = total * arr[i];
+    }
+  }
+  return total;
 };
 
 // Do not edit below this line
