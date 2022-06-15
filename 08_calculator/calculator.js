64154fac7af6f2b2193cb4567d055b78f06d7b1a
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-15 14:25:37 +0200

    Pass test 5, 6, 7 and 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,10 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.forEach(number => total += number);
+  return total;
 };
 
 const multiply = function() {
