734b7326621b9e247d76f481c2dc218c07a9238e
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-15 21:12:55 +0200

    Pass all tests
    This didn't take me long to solve.
    The push method was still fresh in my mind.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(n) {
+    Number(n);
+    if(n < 0) return 'OOPS';
+    let arr = [1, 1];
+    for(let i = 1; i < n; i++) {
+        let fib = arr[i] + arr[i - 1];
+        arr.push(fib);
+    }
+    return arr[n - 1];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
