42d2e3b3853f5c5121579f11ab0585c448e09191
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-16 12:25:43 +0200

    Pass test 1
    Figuring out a way how to make use of reduce and
    understanding the method itself took a long time.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const totalValue = people.reduce((total, item) => {
+        if(item.yearOfDeath - item.yearOfBirth > total) total = item.yearOfDeath - item.yearOfBirth;
+        return total;
+    }, 0);
+    let index;
+    people.forEach(item => {
+        if((item.yearOfDeath - item.yearOfBirth) === totalValue) index = people.indexOf(item);
+    });
+    return people[index];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
