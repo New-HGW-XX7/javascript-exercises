0c58aff10a468ae8999a46a4a21b37ac0284d43b
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-16 10:38:38 +0200

    Pass test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let titleArray = [];
+    arr.forEach(book => titleArray.push(book.title));
+    return titleArray;
 };
 
 // Do not edit below this line
